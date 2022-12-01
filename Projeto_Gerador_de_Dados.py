24bceafff4e6b6441f3a65741d9025d5649061de
alysson Gabriel <alyssonsmartcell@gmail.com>
2022-11-30 22:26:06 -0300

    modified:   Projeto_Gerador_de_Dados.py

---
+++ Projeto_Gerador_de_Dados.py
@@ -0,0 +1,11 @@
+print("""
+Bem-vindo ao gerador de dados para testes!
+-----------------------------------------------------------
+Com este gerador e possivel gerar os seguintes dados:
+[1] Nome
+[2] Email
+[3] Telefone
+[4] Cidade
+-----------------------------------------------------------
+OBS:Digite as opções separando-as po uma ',' ex: (1,2,3...)
+Digite uma ou mais opções: """)
