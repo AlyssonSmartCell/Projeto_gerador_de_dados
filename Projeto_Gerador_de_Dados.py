335a39ea44ec17b990d337a931dc9acafadbf7ac
alysson Gabriel <alyssonsmartcell@gmail.com>
2022-12-01 23:07:33 -0300

    ATUALIZAÇÃO 1

--- Projeto_Gerador_de_Dados.py
+++ Projeto_Gerador_de_Dados.py
@@ -1,3 +1,4 @@
+from random import randint
 print("""
 Bem-vindo ao gerador de dados para testes!
 -----------------------------------------------------------
@@ -8,4 +9,31 @@ Com este gerador e possivel gerar os seguintes dados:
 [4] Cidade
 -----------------------------------------------------------
 OBS:Digite as opções separando-as po uma ',' ex: (1,2,3...)
-Digite uma ou mais opções: """)
+""")
+
+opçoes = input("Digite uma ou mais opções: ")
+
+nomes = ["Alysson", "nathalia", "selma", "yasmin", "gustavo"]
+emails = ["alysson@gmail.com", "selma@gmail.com", "gustavo@gmail.com", "nathalia@gmail.com", "yasmin@gmail.com"]
+telefones = ["7865594-8776", "9806654-5467", "1238897-9876", "4398806-6484", "679 0054- 4678"]
+cidade = ["Londrina", "Maringa", "cascavel", "Sao Paulo", "Jundiai"]
+
+
+
+tela_inicial = True
+while tela_inicial == True:
+    if opçoes == "1":
+        aleatorio = randint(0, 4)
+        print(nomes[aleatorio])
+        break
+    
+    elif opçoes == "2":
+        aleatorio = randint(0, 4)
+        print(emails[aleatorio])
+        break
+
+    
+
+
+deseja_continuar = input("Deseja continuar (s/n): ")
+
---
+++ TESTES.py
@@ -0,0 +1,6 @@
+from random import randint 
+
+s = []
+for c in range(0,10):
+    s.append ( input("teste") )  
+    print(s)
